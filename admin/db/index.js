37c4de6d03d449b6ee78e1c335f593e29549d9c8
Feng373712195 <373712195@qq.com>
2019-07-28 00:48:17 +0800

    修改数据库地址

--- admin/db/index.js
+++ admin/db/index.js
@@ -4,7 +4,8 @@ const redis = require('redis');
 const config = require('../config');
 
 mongoose.Promise = global.Promise;
-let dbClient = mongoose.createConnection('localhost','blog')
-let redisClient = redis.createClient(config.redisPort,'localhost')
+let dbClient = mongoose.createConnection('106.52.59.218','blog')
+// let redisClient = redis.createClient(config.redisPort,'localhost')
 
-module.exports =  { dbClient,redisClient };
+// ,redisClient
+module.exports =  { dbClient };
