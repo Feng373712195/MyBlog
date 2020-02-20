4313d0d8e0dddfba551e465af31451873cc96d6e
Feng373712195 <373712195@qq.com>
2020-02-20 09:48:25 +0800

    update

--- src/assets/js/antdIcons.js
+++ src/assets/js/antdIcons.js
@@ -1,6 +1,5 @@
-export { 
-    MenuOutline,
-    DoubleLeftOutline,
-    DoubleRightOutline,
-    VerticalLeftOutline,
-    VerticalRightOutline } from '@ant-design/icons';
+export {default as MenuOutline} from '@ant-design/icons/lib/outline/MenuOutline';
+export {default as DoubleLeftOutline} from '@ant-design/icons/lib/fill/DoubleLeftOutline';
+export {default as DoubleRightOutline} from '@ant-design/icons/lib/fill/DoubleRightOutline';
+export {default as VerticalLeftOutline} from '@ant-design/icons/lib/fill/VerticalLeftOutline';
+export {default as VerticalRightOutline} from '@ant-design/icons/lib/fill/VerticalRightOutline';
